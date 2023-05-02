ae16ebc7597cea0874902a1d9817aa08eeebcd9b
Rustam Shasaidov <luckyhecz@gmail.com>
2023-05-02 12:03:25 +0300

    feat: first task (2 мая 2023 г. 9:03:25)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+const fs = require('fs');
+const path = require('path');
+
+
+const direction = path.resolve('01-read-file', 'text.txt');
+const stream = fs.createReadStream(direction, 'utf-8');
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message));
+// fs.readFile(
+//   path.join(__dirname, 'text.txt'),
+//   'utf-8',
+//   (err, data) => {
+//       if (err) throw err;
+//       console.log(data);
+//   }
+// );
